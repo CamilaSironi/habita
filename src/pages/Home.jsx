110fbde57d4080343c91481514562b95208f2cfe
Camila <sironicamila98@gmail.com>
2026-01-11 19:36:34 -0300

    add Skeletons to Home loading

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,19 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 import { properties } from "../data/properties";
+import PropertyCardSkeleton from "../components/PropertyCardSkeleton";
 import PropertyCard from "../components/PropertyCard";
 import MainLayout from "../layouts/MainLayout";
 
 export default function Home() {
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1200);
+      return () => clearTimeout(timer);
+  }, []);
 
   return (
     <MainLayout>
@@ -12,12 +22,20 @@ export default function Home() {
         <h2>Most Popular</h2>
 
         <div className="properties-grid">
-          {properties.slice(0, 3).map((property) => (
-            <PropertyCard
-              key={property.id}
-              property={property}
-            />
-          ))}
+          {loading ? (
+            Array.from({ length: 6 }).map((_, i) => (
+              <PropertyCardSkeleton key={i} />
+            ))
+            ) : properties.length === 0 ? (
+              <p>No properties found.</p>
+            ) : (
+              properties.slice(0, 3).map((property) => (
+                <PropertyCard
+                  key={property.id}
+                  property={property}
+                />
+            ))
+          )}
         </div>
         
       </section>
